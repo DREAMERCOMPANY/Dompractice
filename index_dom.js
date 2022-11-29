e62b73b003a4ff1f986575bda61b5368f42932bb
DREAMERCOMPANY <jhon98-ra@hotmail.com>
2022-11-29 10:24:39 -0500

    Digital clock and alarm

--- index_dom.js
+++ index_dom.js
@@ -1,8 +1,17 @@
+//import function "hamburguerMenu" from file "menu_hamburguesa.js"
 import hamburguerMenu from "./menu_hamburguesa.js";
-
+//two functions are imported; "digitalClock" and "alarm" from the clock.js file
+import {digitalClock,alarm } from "./reloj.js"; 
 const d = document
 
-d.addEventListener("DOMContentLoaded", e=>{
+d.addEventListener("DOMContentLoaded", e=>{ // document content loaded event is "accessed" and "used" with different functions
+    //"hamburguerMenu" function is accessed and its parameters are established
     hamburguerMenu(".panel-btn", ".panel", ".menu a")
+    //"digitalClock" function is accessed and its parameters are established
+    digitalClock("#reloj","#activar-reloj","#desactivar-reloj") 
+    //"alarm" function is accessed and parameters are established
+    alarm("./Assets/alarma.mp3","#activar-alarma","#desactivar-alarma")
 })
+
+
 
