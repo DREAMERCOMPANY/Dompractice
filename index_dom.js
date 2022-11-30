86600eb44078b01014028162c63b53a752857231
DREAMERCOMPANY <jhon98-ra@hotmail.com>
2022-11-30 11:56:15 -0500

    Countdown

--- index_dom.js
+++ index_dom.js
@@ -1,9 +1,11 @@
 //import function "hamburguerMenu" from file "menu_hamburguesa.js"
+import countdown from "./cuenta_regresiva.js";
 import hamburguerMenu from "./menu_hamburguesa.js";
 //two functions are imported; "digitalClock" and "alarm" from the clock.js file
 import {digitalClock,alarm } from "./reloj.js"; 
 import { shortcuts } from "./teclado.js";
 import { moveBall } from "./teclado.js";
+
 const d = document
 
 d.addEventListener("DOMContentLoaded", e=>{ // document content loaded event is "accessed" and "used" with different functions
@@ -13,6 +15,7 @@ d.addEventListener("DOMContentLoaded", e=>{ // document content loaded event is 
     digitalClock("#reloj","#activar-reloj","#desactivar-reloj") 
     //"alarm" function is accessed and parameters are established
     alarm("./Assets/alarma.mp3","#activar-alarma","#desactivar-alarma")
+    countdown("countdown","May 29,2023 11:41:22","Feliz Cumpleaños Dreamer 🐒")
 })
 
 
