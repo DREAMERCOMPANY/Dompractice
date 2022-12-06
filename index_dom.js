5771037ee7791c7b609838b81ddd71ec96aea996
DREAMERCOMPANY <jhon98-ra@hotmail.com>
2022-12-06 15:01:00 -0500

    webcam detection

--- index_dom.js
+++ index_dom.js
@@ -4,6 +4,7 @@ import countdown from "./cuenta_regresiva.js";
 import darktheme from "./dark_mode.js";
 import userDeviceInfo from "./deteccion_dispositivos.js";
 import networkStatus from "./deteccion_red.js";
+import webCam from "./deteccion_webcam.js";
 import hamburguerMenu from "./menu_hamburguesa.js";
 import responsiveMedia from "./objeto_responsive.js";
 import responsiveTester from "./prueba_responsive.js";
@@ -34,6 +35,7 @@ d.addEventListener("DOMContentLoaded", e=>{ // document content loaded event is 
     `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d429176.33072262927!2d-97.01174406690926!3d32.82035249042345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864c19f77b45974b%3A0xb9ec9ba4f647678f!2sDallas%2C%20TX%2C%20USA!5e0!3m2!1sen!2sco!4v1670012834916!5m2!1sen!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`)
     responsiveTester("responsive-tester")  
     userDeviceInfo("user-device")
+    webCam("webcam")
     
 
 })
